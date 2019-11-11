7933918eb79a7b0f8ca52b58c30782f98e84a54e
TaghreedNageeb <50971460+TaghreedNageeb@users.noreply.github.com>
2019-11-11 16:16:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-document.addEventListener('load', function() {
+window.addEventListener('load', function() {
     fetch("https://api.myjson.com/bins/15g2j6")
         .then(res => res.json())
         .then(obj => {
-            let products = document.querySelector("#products");
-            let card = document.createElement('div');
-            card.className="card";
-            let image = document.createElement('img');
-            image.className ="card-img-top";
-            image.src = obj.image;
-            let small = document.createElement('div');
-            small.className ="card-body";
-            let title = document.createElement('h5');
-            title.innerText= obj.name;
-            title.className ="card-title";
-            let paragraph = document.createElement('p');
-            paragraph.innerText = obj.description;
-            paragraph.className ="card-text";
-            let link = document.createElement('a');
-            link.className ="btn btn-primary";
-            card.appendChild(image);
-            card.appendChild(small);
-            small.appendChild(title);
-            small.appendChild(paragraph);
-            small.appendChild(link);
-            products.appendChild(card);
+            for (let i = 0; i < obj.length; i++) {
+                let products = document.querySelector("main");
+                let card = document.createElement('div');
+                card.className = "card";
+                let image = document.createElement('img');
+                image.className = "card-img-top";
+                image.src = obj[i].image;
+                let small = document.createElement('div');
+                small.className = "card-body";
+                let title = document.createElement('h5');
+                title.innerText = obj[i].name;
+                title.className = "card-title";
+                let paragraph = document.createElement('p');
+                paragraph.innerText = obj[i].description;
+                paragraph.className = "card-text";
+                let link = document.createElement('a');
+                link.className = "btn btn-primary";
+                card.appendChild(image);
+                card.appendChild(small);
+                small.appendChild(title);
+                small.appendChild(paragraph);
+                small.appendChild(link);
+                products.appendChild(card);
+            }
 
         })
         .catch(error => {
+            console.log(error)
 
         });
 })
