1edb5ff4948f7cce6e200f73f8b12e76e9853ea7
TaghreedNageeb <50971460+TaghreedNageeb@users.noreply.github.com>
2019-11-11 15:36:38 +0300

    adding new elements with js

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-document.addEventListener('load',function(){
+document.addEventListener('load', function() {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(obj => {
-        
-        
-    })
-    .catch(error => {
-        
-    } );
+        .then(res => res.json())
+        .then(obj => {
+            let card = document.createElement('div');
+            let image = document.createElement('img');
+            let small = document.createElement('div');
+            let title = document.createElement('h5');
+            let paragraph = document.createElement('p');
+            let link = document.createElement('a');
+            card.appendChild(image);
+            card.appendChild(small);
+            small.appendChild(title);
+            small.appendChild(paragraph);
+            small.appendChild(link);
+
+        })
+        .catch(error => {
+
+        });
 })
+
+
+// function creation() {
+//     let card = document.createElement('div');
+//     let image = document.createElement('img');
+//     let small = document.createElement('div');
+//     let title = document.createElement('h5');
+//     let paragraph = document.createElement('p');
+//     let link = document.createElement('a');
+//     card.appendChild(image);
+//     card.appendChild(small);
+//     small.appendChild(title);
+//     small.appendChild(paragraph);
+//     small.appendChild(link);
+
+// }
